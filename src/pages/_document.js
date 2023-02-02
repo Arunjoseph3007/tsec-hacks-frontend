b04a6290db54d8934f1872e73bfa3fc5ada8249b
Anup Joseph <arunjoseph3007@gmail.com>
2023-02-02 11:19:40 +0530

    Project init

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,19 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html data-theme="corporate" lang="en">
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/@photo-sphere-viewer/core/index.min.css"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
+        <div style={{ width: "400px", height: "300px" }} id="panorama-view" />
       </body>
     </Html>
-  )
+  );
 }
