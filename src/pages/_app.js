bdb08a149374960458575938ba546bcad65996d1
Anup Joseph <arunjoseph3007@gmail.com>
2023-02-02 18:02:29 +0530

    Create room form done

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,18 +1,31 @@
 import AuthProvider from "@/context/userContext";
+import { ToastContainer } from "react-toastify";
 import "@/styles/globals.css";
 import "@/styles/Navbar.css";
 import "@/styles/Hero.css";
 import "@/styles/Footer.css";
-import "@/styles/Hero2.css"
-import "@/styles/Awards.css"
-import "@/styles/Featured.css"
-import "@/styles/Features.css"
-
+import "@/styles/Hero2.css";
+import "@/styles/Awards.css";
+import "@/styles/Featured.css";
+import "@/styles/Features.css";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function App({ Component, pageProps }) {
   return (
     <AuthProvider>
       <Component {...pageProps} />
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
     </AuthProvider>
   );
 }
